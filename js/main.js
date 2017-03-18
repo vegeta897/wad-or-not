2f3ba280b3470c1eabb2f3a94f5e2997619ff2d7
Devin Spikowski <vegeta897@gmail.com>
2017-03-18 04:54:11 -0400

    Add webpack

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.body.innerHTML = this.responseText;
+        document.getElementById('main').innerHTML = this.responseText;
     }
 };
 xmlhttp.open('GET', 'php/images.php', true);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    context: path.resolve(__dirname, 'js'),
+    entry: './main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'js'),
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['es2015', { modules: false }]
+                    ]
+                }
+            }]
+        }]
+    }
+};
+
+module.exports = config;
